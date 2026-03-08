f312c6c65a714d8074d7414e31ad5d0e39dbc964
Ganesh Ji Patel <161694533+03Patel@users.noreply.github.com>
2026-03-08 10:37:25 +0530

    Update axios.tsx

--- frontend/src/api/axios.tsx
+++ frontend/src/api/axios.tsx
@@ -5,11 +5,6 @@ const baseURL =
     ? "http://localhost:5000/api"
     : "https://highway-safary.onrender.com";
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 454abf9b62275ebc2b822cec0c99593621deb3bb
 
 const API = axios.create({
   baseURL,
