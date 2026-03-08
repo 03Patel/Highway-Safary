5925ae7a56b736163d21e316676742b9f9bab20b
Ganesh  ji patel <ganeshjipatel108@gmail.com>
2026-03-08 10:15:16 +0530

    second

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,24 +1,12 @@
 import axios from "axios";
-
-<<<<<<< HEAD
-
-const baseURL =
-  window.location.hostname === "localhost"
-    ? "http://localhost:5000/api"
-    : "";
-=======
 const baseURL =
   window.location.hostname === "localhost"
     ? "http://localhost:5000/api"
     : "https://highway-safary.onrender.com";
->>>>>>> 62f002e22f1acae979f4faca1986da2c81a03013
+
 
 const API = axios.create({
   baseURL,
 });
 
 export default API;
-<<<<<<< HEAD
-
-=======
->>>>>>> 62f002e22f1acae979f4faca1986da2c81a03013
