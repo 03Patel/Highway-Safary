91f445c692fd9d7f2bd6e184a5aca0731c5c8a3b
Ganesh Ji Patel <161694533+03Patel@users.noreply.github.com>
2026-03-13 12:42:33 +0530

    Update axios.tsx

--- frontend/src/api/axios.tsx
+++ frontend/src/api/axios.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const baseURL =
   window.location.hostname === "localhost"
     ? "http://localhost:5000/api"
-    : "https://highway-safarybackend-85zs.onrender.com/api";
+    : "https://highway-safarybackend-fsoz.onrender.com";
 
 
 const API = axios.create({
